d1aaaba52c3ec5ec20e4bc3dcb387d9eae065a3d
ViktoriyaKkkk <62558636+ViktoriyaKkkk@users.noreply.github.com>
2023-06-16 02:28:34 +0300

    try to fix cors

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express()
 const server = http.createServer(app)
 app.use(cors())
 app.use(function(req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Origin', 'https://mellow-puffpuff-65cfcb.netlify.app/');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 	res.setHeader('Access-Control-Allow-Credentials', true);
