c926c4129b47948b56eda968e7bcc7f960b28820
ViktoriyaKkkk <62558636+ViktoriyaKkkk@users.noreply.github.com>
2023-06-15 22:04:26 +0300

    once again

--- index.js
+++ index.js
@@ -13,13 +13,27 @@ const DB_URL = 'mongodb+srv://root:gemini2903@cluster0.5cjniay.mongodb.net/?retr
 const app = express()
 const server = http.createServer(app)
 // app.use(cors())
-app.use(function(req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-	res.setHeader('Access-Control-Allow-Credentials', true);
-	next();
-});
+// app.use(function(req, res, next) {
+// 	res.setHeader('Access-Control-Allow-Origin', '*');
+// 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+// 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+// 	res.setHeader('Access-Control-Allow-Credentials', true);
+// 	next();
+// });
+const corsOpts = {
+	origin: '*',
+
+	methods: [
+		'GET',
+		'POST',
+	],
+
+	allowedHeaders: [
+		'Content-Type',
+	],
+};
+
+app.use(cors(corsOpts));
 app.use(express.json())
 app.use('/api', router)
 const io = new Server(server, {
